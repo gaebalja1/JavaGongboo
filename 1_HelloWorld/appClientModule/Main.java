bb969da34e6848a7ab36d30eb59df6a7996e0036
gaebalja1 <gaebalja@outlook.com>
2016-04-13 15:12:18 -0700

    Hello World 출력
    Hello World 출력 줄 추가

--- 1_HelloWorld/appClientModule/Main.java
+++ 1_HelloWorld/appClientModule/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 자바로 출력한 첫 번째 줄. 오이!
+		System.out.print("Hello World");
 	}
 
 	/* (non-Java-doc)
